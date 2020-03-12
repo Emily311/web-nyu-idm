23083d68a2f86cd6e4c21324cf264d51eb23e1ba
王一旻 <wangyimin@MacBook-Pro-3.local>
2020-03-12 16:06:42 -0400

    second commit

--- javascript-playground/challenge.js
+++ javascript-playground/challenge.js
@@ -3,12 +3,15 @@
 let myAlphabet = ['A', 'B', 'C', 'D','E','F','G'];
 
 // 1. What is the length of the array?
+console.log(namesArray.length);
 // push your edits to github!!
 
 // 2. Write a function called myAlphabetLength which console.logs the length of the array
+console.log(myAlphabetLength.length);
 // push your edits to github!!
 
 // 3. Within the function also use an if-conditional statement that checks if the number of items within the array are less than 4
+
 // push your edits to github!!
 
 // 4. Declare and initialize an array called 'colors' with 5 different string values representing the names of 5 colors of your choosing
